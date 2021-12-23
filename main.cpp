5a1052fa84f2e942d5cee32727cfd4a601c0e066
Alexandru <bulzanalexandru@gmail.com>
2021-12-23 21:28:27 +0200

    second commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
+#include<fstream>
+#include<string>
 
 void main()
 {
-	std::cout << "Hello CMake";
+	std::fstream fs("Resources/Greeting.txt");
+
+	if (fs.is_open())
+	{
+		std::string line;
+		std::getline(fs, line);
+		std::cout << line << '\n';
+	}
+	else
+	{
+		std::cout << "Error 2";
+	}
 }
